1cd360d47b8aae04916dd0043d95094d509a1181
Tyler Pollom <tylerpollom@protonmail.com>
2023-03-09 17:28:35 -0700

    Example Failing Test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,6 +11,17 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList list;
 
-
+    public CustomList mockCityList() {
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+    @Test
+    public void testAddCity() {
+       list = mockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Edmonton", "AB"));
+        assertEquals(1, listSize);
+    }
 }
